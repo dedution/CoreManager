eae1b3f71ec502a90baa75421545598edb7f9854
Fábio Barbosa <fabiobarbosa96@hotmail.com>
2024-02-28 01:15:32 +0000

    Updates to Debug Manager Module
    Added matrix scalling logic.
    Left some comments about what TODO next.

--- Runtime/Scripts/Core/Modules/DebugManager.cs
+++ Runtime/Scripts/Core/Modules/DebugManager.cs
@@ -5,17 +5,51 @@ namespace core.modules
 {
     public class DebugManager : BaseModule
     {
-        public bool fullscreenBool = false;
-        private Vector2 scrollPosition;
+        // TODO
+        // Load layout and submenus from json file (load a default one from this package, or ignore and load one from the game logic if it exists)
+        // Build the entire layout only once
+        // Find a dynamic way to execute logic from the layout. Try to make this module as dynamic and reusable as possible
+        // (ex. button has a string callback for "GameManager.NeedThisCall" and Debug Manager needs to parse that callback)
+        // careful with callbacks for classes in inaccessible namespaces?
+
+        /* public bool fullscreenBool = false;
+        private Vector2 scrollPosition; */
+        private Matrix4x4 currentMatrix;
+        private bool m_isActive = false;
+
+        public bool isActive
+        {
+            get
+            {
+                return m_isActive;
+            }
+
+            set 
+            { 
+                m_isActive = value;
+
+                // Wont this interfere?
+                if(m_isActive)
+                    SetMatrix();
+                else
+                    ResetMatrix();
+            }
+        }
+        
+        // Default size of debug manager
+        private Vector2 nativeSize = new Vector2(1280, 720);
 
         public override void onInitialize()
         {
-            
+            currentMatrix = GUI.matrix;
         }
 
-        private void DrawMenuTest()
+        private void DrawDebugMenu()
         {
-            GUILayout.BeginArea(new Rect(0, 0, 400, 600), "", "box");
+            if(!isActive)
+                return;
+            
+            /* GUILayout.BeginArea(new Rect(0, 0, 400, 600), "", "box");
             scrollPosition = GUILayout.BeginScrollView(
             scrollPosition, GUILayout.Width(400), GUILayout.Height(500));
             GUILayout.Box("Settings:");
@@ -36,17 +70,23 @@ namespace core.modules
             GUILayout.Label("BUILD ID: XXXXXXX");
             GUILayout.Label("BUILD DATE: 01-01-0101");
 
-            GUILayout.EndArea();
+            GUILayout.EndArea(); */
+        }
+
+        private void SetMatrix()
+        {
+            Vector3 scale = new Vector3 (Screen.width / nativeSize.x, Screen.height / nativeSize.y, 1.0f);
+            GUI.matrix = Matrix4x4.TRS (new Vector3(0, 0, 0), Quaternion.identity, scale);
+        }
+
+        private void ResetMatrix()
+        {
+            GUI.matrix = currentMatrix;
         }
 
         public override void OnGUI()
         {
-            //DrawMenuTest();
-        }
-
-        public void Hellow()
-        {
-            Debug.Log("Hellow from Debug Manager.");
+            DrawDebugMenu();
         }
     }
 }
