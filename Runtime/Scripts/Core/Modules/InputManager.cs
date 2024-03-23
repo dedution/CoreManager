35e1b6d9dcdafe24c8d2fb01b996c01191789393
Fábio Barbosa <fabiobarbosa96@hotmail.com>
2024-03-23 16:37:54 +0000

    Changes to InputManager
    Commented logic from Input Manager in favor of unitys Input System.
    Added assembly reference of unitys new input system to Core Manager.

--- Runtime/Scripts/Core/Modules/InputManager.cs
+++ Runtime/Scripts/Core/Modules/InputManager.cs
@@ -7,8 +7,8 @@ namespace core.modules
 {
     public class InputManager : BaseModule
     {
-        private delegate void ButtonDelegate();
-        private Dictionary<string, Event> m_ButtonDelegates = new Dictionary<string, Event>();
+        /* private delegate void ButtonDelegate();
+        private Dictionary<string, Action> m_ButtonDelegates = new Dictionary<string, Action>(); */
 
         private static InputManager _instance = null;
 
@@ -19,21 +19,34 @@ namespace core.modules
 
         public override void onInitialize()
         {
-
+            // Load Input configuration and populate dictionary of button actions 
+            // m_ButtonDelegates.Add("", delegate() {});
         }
 
-        public override void UpdateModule()
+        /* public override void UpdateModule()
         {
-            // 
+
         }
 
         // TODO
         // Dynamic handling of inputs for controllers and prompt icon packs
         // Loading configurations from external resources, allowing for complete sets of inputs without touching the codebase
+        // m_ButtonDelegates.Add("Jump", delegate() { onJump(); });
+
         public static void BindToButton(string _buttonID, Action _bindAction)
         {
-            /* if(_instance.m_ButtonDelegates.ContainsKey(_buttonID))
-                _instance.m_ButtonDelegates[_buttonID] += _bindAction; */
+            if(_instance.m_ButtonDelegates.ContainsKey(_buttonID) && _instance.m_ButtonDelegates[_buttonID] != null)
+                _instance.m_ButtonDelegates[_buttonID] += _bindAction;
+
+            Debug.LogWarningFormat("Failed to bind action to button! [{}]", _buttonID);
         }
+
+        public static void UnbindToButton(string _buttonID, Action _bindAction)
+        {
+            if(_instance.m_ButtonDelegates.ContainsKey(_buttonID) && _instance.m_ButtonDelegates[_buttonID] != null)
+                _instance.m_ButtonDelegates[_buttonID] -= _bindAction;
+            
+            Debug.LogWarningFormat("Failed to unbind action to button! [{}]", _buttonID);
+        } */
     } 
 }
